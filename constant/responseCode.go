d7dbe08097c23655fc4ccc5c9c1b8854f97b269f
ivorysillegalss <chenzczczc@gmail.com>
2024-02-02 02:07:42 +0800

    补充错误类型DTO类 修改至controller层接口中 优化接口中错误处理

---
+++ constant/responseCode.go
@@ -0,0 +1,23 @@
+package constant
+
+const (
+	// CreateNewChat 开启聊天
+	StartChatSuccess = 100
+	StartChatError   = 101
+
+	//InitChat 主页面渲染
+	ShowChatHistorySuccess = 110
+	ShowChatHistoryError   = 111
+
+	//CallBot 调用机器人
+	CallBotSuccess = 120
+	CallBotError   = 121
+
+	//AdminGetBot 管理员获取机器人信息
+	AdminGetBotSuccess = 900
+	AdminGetBotError   = 901
+
+	//AdminSaveNewBot 管理员创建新机器人
+	AdminSaveNewBotSuccess = 910
+	AdminSaveNewBotError   = 911
+)
