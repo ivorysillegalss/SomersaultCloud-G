0a1f15c98dcb57565b5585230d416797e8097080
ivorysillegalss <chenzczczc@gmail.com>
2024-03-14 13:56:43 +0800

    完善保存历史记录与获取历史记录 表连接查询效果未知 分享功能

--- go.mod
+++ go.mod
@@ -29,6 +29,7 @@ require (
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
--- service/mainPageService.go
+++ service/mainPageService.go
@@ -1,9 +1,30 @@
 package service
 
-import "mini-gpt/models"
+import (
+	"github.com/google/uuid"
+	"mini-gpt/models"
+	"mini-gpt/utils/redisUtils"
+	"time"
+)
 
 // 渲染主页chat标题等
 func InitMainPage(userId int) ([]*models.Chat, error) {
 	chatTitles, err := models.ShowChatTitle(userId)
 	return chatTitles, err
+}
+
+func ShareChatHistory(chatId int, day int) string {
+	uuidV4 := uuid.New()
+	u := uuidV4.String()
+	duration := time.Duration(day) * 24 * time.Hour
+	//表示保存到数据库中限时3天
+
+	//异步保存 先返回对应sk
+	go setHistory(chatId, u, duration)
+	return u
+}
+
+func setHistory(chatId int, u string, duration time.Duration) {
+	//TODO 这里补充代码查询相关历史记录复制一份
+	_ = redisUtils.SetStructWithExpire(u, chatId, duration)
 }
