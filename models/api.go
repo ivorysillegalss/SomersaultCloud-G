e91702f6f6bfab582765caed458b3222e48cf1e0
ivorysillegalss <chenzczczc@gmail.com>
2024-04-23 23:46:21 +0800

    大幅修改原有结构，根据模型分配方法。原ApiRequestMessage修改为接口，适配多种响应格式的信息。历史记录的组装修改TBD 若增加类型的信息可以直接实现request()空方法。 设计模式用的非常不成熟。整个运行逻辑首先从配置文件中获取模型的分类信息。现有文本补全和默认模型两种，详见constant中。并根据模型的不同，类型断言后包装对应信息。发起请求后的响应信息格式也不相同。两个模型需要两套独立的方法。还是非常不成熟。 修改后，仍然以传递ApiRequestMessage为主，若要拓展模型，从chat.go中开始继承方法开始。相比于以前的屎山来说好看一点，增加可拓展性。

--- models/api.go
+++ models/api.go
@@ -1,21 +1,52 @@
 package models
 
-import "time"
+import (
+	"encoding/json"
+	"mini-gpt/constant"
+)
 
-// CompletionRequest 定义请求结构体
+type BaseModel interface {
+	completion()
+}
+
+func (t *TextCompletionResponse) completion() {}
+func (c *ChatCompletionResponse) completion() {}
+
+type ApiRequestMessage interface {
+	request()
+}
+
+func (t *TextCompletionRequest) request() {}
+func (c *ChatCompletionRequest) request() {}
+
+// ChatCompletionRequest 定义大语言请求结构体
+type ChatCompletionRequest struct {
+	CompletionRequest
+	Messages []Message `json:"messages"`
+}
+
+type Message struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+// TextCompletionRequest 定义文本补全请求结构体
+type TextCompletionRequest struct {
+	CompletionRequest
+	Prompt string `json:"prompt"`
+}
+
 type CompletionRequest struct {
-	Model     string `json:"model"`
-	Prompt    string `json:"prompt"`
 	MaxTokens int    `json:"max_tokens"`
+	Model     string `json:"model"`
 }
 
 // CompletionResponse 定义响应结构体
 type CompletionResponse struct {
-	Id         string   `json:"id"`
-	Object     string   `json:"object"`
-	CreateTime int64    `json:"created"`
-	Model      string   `json:"model"`
-	Choices    []Choice `json:"choices"`
+	Id         string `json:"id"`
+	Object     string `json:"object"`
+	CreateTime int64  `json:"created"`
+	Model      string `json:"model"`
 	Usage      struct {
 		PromptTokens     int64 `json:"prompt_tokens"`
 		CompletionTokens int64 `json:"completion_tokens"`
@@ -24,10 +55,27 @@ type CompletionResponse struct {
 }
 
 // openAI返回的json中请求体中的文本是一个数组
-type Choice struct {
-	Text         string `json:"text"`
-	Index        int64  `json:"index"`
-	FinishReason string `json:"finish_reason"`
+// 文本补全 响应数据格式
+type TextCompletionResponse struct {
+	CompletionResponse
+	Choices []struct {
+		Text         string `json:"text"`
+		Index        int64  `json:"index"`
+		FinishReason string `json:"finish_reason"`
+	} `json:"choices"`
+}
+
+// chat模型响应数据格式
+type ChatCompletionResponse struct {
+	CompletionResponse
+	Choices []struct {
+		Index   int `json:"index"`
+		Message struct {
+			Role    string `json:"role"`
+			Content string `json:"content"`
+		} `json:"message"`
+		FinishReason string `json:"finish_reason"`
+	} `json:"choices"`
 }
 
 func (c CompletionResponse) ToString() {
@@ -40,12 +88,12 @@ type GenerateMessage struct {
 	FinishReason string
 }
 
-// ApiRequestMessage 前端发请求json格式
-type ApiRequestMessage struct {
-	InputPrompt string `json:"prompt"`
-	Model       string `json:"model"`
-	MaxToken    int    `json:"max_token"`
-}
+//// ApiRequestMessage 前端发请求json格式
+//type ApiRequestMessage struct {
+//	InputPrompt string `json:"prompt"`
+//	Model       string `json:"model"`
+//	MaxToken    int    `json:"max_token"`
+//}
 
 // 异常返回空对象
 func ErrorGeneration() *GenerateMessage {
@@ -71,29 +119,79 @@ type ExecuteStatus struct {
 }
 
 // 系统错误
-func ErrorCompletionResponse() *CompletionResponse {
-	return &CompletionResponse{
-		CreateTime: time.Now().Unix(),
-		Choices:    nil,
-	}
+func ErrorCompletionResponse() BaseModel {
+	return nil
 }
 
 // 运行错误 用户的锅
-func ExceptionCompletionResponse(exceptionMessage string) *CompletionResponse {
+func ExceptionCompletionResponse(exceptionMessage string) BaseModel {
 	//这里草草包装了一下 未测试 感觉还可以优化
-	var choices *[]Choice
-	c := &Choice{
-		Text:         exceptionMessage,
-		FinishReason: "exception",
-	}
-	_ = append(*choices, *c)
-	return &CompletionResponse{
-		CreateTime: time.Now().Unix(),
-		Choices:    *choices,
-	}
+	return nil
 }
 
 // 错误请求信息
-func ErrorApiRequestMessage() *ApiRequestMessage {
-	return new(ApiRequestMessage)
+//func ErrorApiRequestMessage() ApiRequestMessage {
+//	return new(ApiRequestMessage)
+//}
+
+// 文本补全模型包装信息 gpt-3.5-turbo-instruct
+func EncodeTextCompletionJsonData(reqMessage ApiRequestMessage) ([]byte, error) {
+	var textCompletion *TextCompletionRequest
+	if t, ok := reqMessage.(*TextCompletionRequest); !ok {
+		return nil, nil
+		//TODO 错误处理
+	} else {
+		textCompletion = t
+	}
+	//这个判空的过程可以优化在结构体 models层中
+	if textCompletion.MaxTokens == constant.ZeroInt {
+		textCompletion.MaxTokens = constant.DefaultMaxToken
+	}
+
+	// 构造请求体
+	data := TextCompletionRequest{
+		CompletionRequest: CompletionRequest{
+			MaxTokens: textCompletion.MaxTokens,
+			Model:     textCompletion.Model,
+		},
+		//Model:     "gpt-3.5-turbo-instruct", // 替换为当前可用的模型
+		Prompt: textCompletion.Prompt,
+	}
+	jsonData, err := json.Marshal(data)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return jsonData, nil
+}
+
+func EncodeChatCompletionJsonData(reqMessage ApiRequestMessage) ([]byte, error) {
+	var chatCompletion *ChatCompletionRequest
+	if c, ok := reqMessage.(*ChatCompletionRequest); !ok {
+		//TODO 错误处理
+		return nil, nil
+	} else {
+		chatCompletion = c
+	}
+	//这个判空的过程可以优化在结构体 models层中
+	if chatCompletion.MaxTokens == constant.ZeroInt {
+		chatCompletion.MaxTokens = constant.DefaultMaxToken
+	}
+
+	// 构造请求体
+	data := ChatCompletionRequest{
+		CompletionRequest: CompletionRequest{
+			MaxTokens: chatCompletion.MaxTokens,
+			Model:     chatCompletion.Model,
+		},
+		Messages: chatCompletion.Messages,
+	}
+	jsonData, err := json.Marshal(data)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return jsonData, nil
 }
