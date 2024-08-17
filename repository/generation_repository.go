1c8176e907255f585c9ae5d18493d8e88df51065
ivorysillegalss <chenzczczc@gmail.com>
2024-08-17 15:33:54 +0800

    维护双哈希表 实现单个field单独设置过期时间

---
+++ repository/generation_repository.go
@@ -0,0 +1,33 @@
+package repository
+
+import (
+	"SomersaultCloud/constant/cache"
+	"SomersaultCloud/domain"
+	"SomersaultCloud/infrastructure/redis"
+	"SomersaultCloud/internal/ioutil"
+	"context"
+	"encoding/json"
+	"strconv"
+)
+
+type generationRepository struct {
+	rcl redis.Client
+}
+
+func (g generationRepository) CacheLuaPollHistory(ctx context.Context, generationResp domain.GenerationResponse) {
+	script, err := ioutil.LoadLuaScript("lua/hash_expired.lua")
+	if err != nil {
+		//TODO 打日志
+	}
+
+	//JSON序列化存储 也许可以改进
+	marshal, _ := json.Marshal(generationResp)
+	err = g.rcl.ExecuteArgsLuaScript(context.Background(), script, []string{cache.ChatGeneration, cache.ChatGenerationExpired}, strconv.Itoa(generationResp.ChatId), string(marshal), cache.ChatGenerationTTL)
+	if err != nil {
+		//同上 TODO 打日志
+	}
+}
+
+func NewGenerationRepository() domain.GenerationRepository {
+	return &generationRepository{rcl: dbs.Redis}
+}
